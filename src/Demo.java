81985537e4aad7eb70bd9b26712aa806e7e0dda8
CHETH Dev <starfreds@gmail.com>
2017-12-12 12:50:21 +0100

    Added 2 arrays subjects1, subjects2

---
+++ src/Demo.java
@@ -0,0 +1,11 @@
+public class Demo {
+    private String gender;
+    private String dob;
+    private String name;
+    private String phoneNo;
+    private String school;
+    private String stateOfOrigin;
+    private String[] subjects;
+    private String[] subjects1;
+    private String[] subjects2;
+}
